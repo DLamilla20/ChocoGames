bce4a2e391ef4573209071dd190d7c3ac0acbe01
DLamilla20 <lamilla.tenazoa@gmail.com>
2023-05-07 17:32:08 -0500

    Juego2deRitmo

---
+++ Assets/Script/Generador1A.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Generador1A : MonoBehaviour
+{
+    public GameObject[] Gene1A;
+    private float tiempoEntreLetras;
+    public float comienzoDeTiempo;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (tiempoEntreLetras <= 0)
+        {
+            int random = Random.Range(0, Gene1A.Length);
+            Instantiate(Gene1A[random],this.transform.position,Quaternion.identity);
+
+            tiempoEntreLetras = comienzoDeTiempo;
+        }
+        else
+        {
+            tiempoEntreLetras -= Time.deltaTime;
+        }
+    }
+}

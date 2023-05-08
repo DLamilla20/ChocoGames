b409972578740d58f860466ccb9bbc30cb030af7
DLamilla20 <lamilla.tenazoa@gmail.com>
2023-05-07 22:35:11 -0500

    Game2Finalizado

---
+++ Assets/Script/JugadorLadoB.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class JugadorLadoB : MonoBehaviour
+{
+    public int puntaje = 0;
+    public GameObject texto;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TextMeshProUGUI etiquetaPuntaje1 = this.texto.GetComponent<TextMeshProUGUI>();
+        etiquetaPuntaje1.text = this.puntaje.ToString();
+    }
+
+    public void PuntajeLadoB()
+    {
+        puntaje++;
+    }
+}

4e2047adf05efd257e71f128e1c399236bd51009
DLamilla20 <lamilla.tenazoa@gmail.com>
2023-06-16 01:26:40 -0500

    test prueba

--- Assets/Script/LogicaGame2/ControladorBaile.cs
+++ Assets/Script/LogicaGame2/ControladorBaile.cs
@@ -6,8 +6,13 @@ public class ControladorBaile : MonoBehaviour
 {
     public static ControladorBaile Instance;
 
-    private Animator animator;
+    private Animator animator1;
+    private Animator animator2;
     // Start is called before the first frame update
+    private void Start()
+    {
+        animator1 = GameObject.Find("Jugador1").GetComponentInChildren<Animator>();
+    }
     private void Awake()
     {
         if (Instance == null)
@@ -20,44 +25,57 @@ public class ControladorBaile : MonoBehaviour
             Destroy(gameObject);
         }
 
-        animator = GameObject.Find("Jugador1").GetComponentInChildren<Animator>();
+        
     }
 
     public void Baile(int i) 
     {
         if (i == 1)
         {
-            print("Hola1");
-            string cArriba = "cArriba";
-            animator.SetBool(cArriba, true);
-            StartCoroutine(Espera(cArriba));
+            if (animator1 != null)
+            {
+                print("Hola1");
+                string cArriba = "cArriba";
+                animator1.SetBool(cArriba, true);
+                StartCoroutine(Espera(cArriba));
+            }
+            
         }
         else if (i == 2)
         {
-            print("Hola2");
-            string cIzquierda = "cIzquierda";
-            animator.SetBool(cIzquierda, true);
-            StartCoroutine(Espera(cIzquierda));
+            if (animator1 != null)
+            {
+                print("Hola2");
+                string cIzquierda = "cIzquierda";
+                animator1.SetBool(cIzquierda, true);
+                StartCoroutine(Espera(cIzquierda));
+            }
         }
         else if (i == 3)
         {
-            print("Hola3");
-            string cAbajo = "cAbajo";
-            animator.SetBool(cAbajo, true);
-            StartCoroutine(Espera(cAbajo));
+            if (animator1 != null)
+            {
+                print("Hola3");
+                string cAbajo = "cAbajo";
+                animator1.SetBool(cAbajo, true);
+                StartCoroutine(Espera(cAbajo));
+            }
         }
         else if (i == 4)
         {
-            print("Hola4");
-            string cDerecha = "cDerecha";
-            animator.SetBool(cDerecha, true);
-            StartCoroutine(Espera(cDerecha));
+            if (animator1 != null)
+            {
+                print("Hola4");
+                string cDerecha = "cDerecha";
+                animator1.SetBool(cDerecha, true);
+                StartCoroutine(Espera(cDerecha));
+            }
         }
     }
 
     public IEnumerator Espera(string tipo) 
     {
         yield return new WaitForSeconds(1.3f);
-        animator.SetBool(tipo, false);
+        animator1.SetBool(tipo, false);
     }
 }
